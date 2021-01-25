f30ad8cef1f7e2aba8fbc7e13a736f434921cbd2
houpai <houpai@qq.com>
2021-01-25 17:17:24 +0800

    commit

--- utils/httpConfig.js
+++ utils/httpConfig.js
@@ -1,3 +1,3 @@
-const serviceHost = process.env.NODE_ENV === "development" ? "http://192.168.1.123/webapp/" : "http://192.168.1.123/webapp/"
+const serviceHost = process.env.NODE_ENV === "development" ? "http://192.168.1.123/webapp/" : "http://60.205.219.160:8080/webapp/"
 
 export {serviceHost}
