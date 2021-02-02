f2831908048d0877e1dcbed4c08b757fc6a8ff60
houpai <houpai@qq.com>
2021-02-02 16:25:52 +0800

    commit

--- src/components/successPage/successPage.vue
+++ src/components/successPage/successPage.vue
@@ -4,16 +4,26 @@
 *@date 2021/02/01 19:28:21
 */
 <template>
-  <div class="successPage_container" v-html="successHtml && successHtml.value">
+  <div class="successPage_container">
+    <div class="success_img">
+      <img src="../../assets/img/logo.png" alt="">
+    </div>
+    <div class="html_box" v-html="successHtml && successHtml.value">
+    </div>
+    <commonFooter></commonFooter>
   </div>
 </template>
 
 <script>
 
 import './successPage.less'
+import commonFooter from "@/components/commonFooter/commonFooter";
 
 export default {
   name: "successPage",
+  components:{
+    commonFooter
+  },
   data() {
     return {
       successHtml:''
